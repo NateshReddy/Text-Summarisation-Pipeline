3508d605581c75323e9c626365b1fa0402a2e8e8
Natesh Reddy <47558025+NateshReddy@users.noreply.github.com>
2024-05-07 19:53:21 -0700

    Update server.py

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ import boto3
 dynamodb = boto3.client('dynamodb', region_name = 'us-east-2')
 table_name = "news-summarisation-data"
 AWS_S3_CREDS = {
-    "aws_access_key_id":"AKIA47CRUPSSRQF7NCXI",
-    "aws_secret_access_key":"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"
+    "aws_access_key_id":"",
+    "aws_secret_access_key":""
 }
 dynamodb = boto3.resource('dynamodb', region_name = 'us-east-2', **AWS_S3_CREDS)
 table = dynamodb.Table("news-summarisation-data")
